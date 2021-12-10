5191389769b326d08f45397fd465a4fcc079093c
Delius Van Den Berg <deliusvdb14@gmail.com>
2021-12-11 00:39:54 +0200

    Player can now jump
    Added Jump and death functionality to player

--- 2D Project V2/Assets/Scripts/PlayerMovement.cs
+++ 2D Project V2/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private BoxCollider2D coll;
+
     private float dirX;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask jumpableGround;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,10 +28,23 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (dirX < -.1f)
+        {
+            sprite.flipX = true;
+        }
+        else if (dirX > .1f)
+        {
+            sprite.flipX = false;
+        }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
