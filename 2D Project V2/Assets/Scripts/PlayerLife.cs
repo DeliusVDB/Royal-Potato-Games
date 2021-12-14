87abc7b3f342b5d12d8712f1d4ea0eea2b0ae2d2
Delius Van Den Berg <deliusvdb14@gmail.com>
2021-12-14 11:51:36 +0200

    Added Background and mountains

--- 2D Project V2/Assets/Scripts/PlayerLife.cs
+++ 2D Project V2/Assets/Scripts/PlayerLife.cs
@@ -22,7 +22,7 @@ public class PlayerLife : MonoBehaviour
 
     private void Update()
     {
-        if (transform.position.y < -20f && !dead)
+        if (transform.position.y < -5f && !dead)
         {
             Die();
         }
