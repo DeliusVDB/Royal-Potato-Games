f7e657c2ecd91b5ff98f3b22c41b6c89e8228da1
Delius Van Den Berg <deliusvdb14@gmail.com>
2021-12-17 13:16:15 +0200

    Added Credits and fixed volume slider not changing volume.

--- 2D Project V2/Assets/Scripts/SettingsMenu.cs
+++ 2D Project V2/Assets/Scripts/SettingsMenu.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
-using UnityEngine.UI;
 using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
     public TMP_Dropdown ResolutionDropdown;
 
     Resolution[] resolutions;
@@ -42,11 +39,6 @@ public class SettingsMenu : MonoBehaviour
     {
         Resolution resolution = resolutions[ResolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-
-    public void SetVolume(float volume)
-    {
-        audioMixer.SetFloat("volume", volume);
     }
 
     public void SetFullscreen (bool isFullscreen)
