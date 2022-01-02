350d67a5edfd1304e4bc9b430a10d5b525f61fd8
Delius Van Den Berg <deliusvdb14@gmail.com>
2022-01-02 22:05:54 +0200

    Game Finished

---
+++ 2D Top Down/Assets/EnemyTargeting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargeting : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+    Transform target;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            target = collision.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            target = null;
+        }
+    }
+}
